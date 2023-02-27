42acbcd71c05a1f6a6853f4b7de8a1821f80337e
jpicados <98858767+jpicados@users.noreply.github.com>
2023-02-27 11:42:03 -0600

    Practica3/EstadoDao

---
+++ src/main/java/com/tienda_k/dao/EstadoDao.java
@@ -0,0 +1,9 @@
+
+package com.tienda_k.dao;
+
+import com.tienda_k.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoDao extends JpaRepository <Estado,Long> {
+    
+}
